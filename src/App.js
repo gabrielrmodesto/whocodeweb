9211f2794ab75c0c1f9776b5d0e0558fdd9512a7
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-01-19 00:44:55 -0300

    Form to register dev

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import './global.css';
-import './App.css';
+import React from "react";
+import "./global.css";
+import "./App.css";
 
 function App() {
   return (
     <div id="app">
       <aside>
-
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="username_github">Username github</label>
+            <input name="username_github" id="username_github" required />
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Techs</label>
+            <input name="techs" id="techs" required />
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
       </aside>
-      <main>
-
-      </main>
+      <main></main>
     </div>
   );
 }
